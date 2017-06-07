7b19fadcaab20ecc14c93f9aaf269bfaa91fb754
Kaushik Vedula <vedula1995@gmail.com>
2017-06-08 04:06:38 +0530

    Article new page is modified with code to simplify the user UI in which to add certain counter of the error messages.`

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,14 @@
 class ArticlesController < ApplicationController
 	def new
+		@article=Article.new
 	end
 	def create
-		@article=Article.new(params.require(:article).permit(:title,:text))
-		
-		@article.save
-		redirect_to @article
+		@article=Article.new(article_params)
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
 	end
 	def index
 		@articles=Article.all
@@ -13,4 +16,7 @@ class ArticlesController < ApplicationController
 	def show
 		@article=Article.find(params[:id])
 	end
+	def article_params
+    	params.require(:article).permit(:title, :text)
+  	end
 end
